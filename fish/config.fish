9159885c5dda68cbd8be9bbb13afe4124df47b9c
Jatin Yadav <jatin.yadav1307@gmail.com>
2019-07-21 23:43:03 +0530

    21 July 2019

--- fish/config.fish
+++
@@ -1,9 +0,0 @@
-# history across fishes
-
-function save_history --on-event fish_preexec
-    history --save
-end
-
-alias hr 'history --merge'
-
-bind \e\[A 'history --merge ; up-or-search'
--- get_files.sh
+++ get_files.sh
@@ -2,6 +2,5 @@
 
 cp -u ~/.tmux.conf ./tmux.conf
 cp -u ~/.config/terminator/config ./
-cp -ur ~/.config/fish ./
 cp -ur ~/.vim ./vim
 cp -u ~/.vimrc ./
